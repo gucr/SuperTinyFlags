3de2f556ab57f81f25e46c2b82d8c5a842e73beb
Guillaume Cier <g@cier.fr>
2023-04-07 18:20:03 +0800

    Adding average & total size in CI script

--- ci/listFlags.js
+++ ci/listFlags.js
@@ -8,14 +8,16 @@ const flagsFolder = path.join(__dirname, '../images')
 const readmePath = path.join(__dirname, '../README.md')
 
 const files = fs.readdirSync(flagsFolder)
-
 const flags = files.filter(file => path.extname(file).toLowerCase() === '.svg')
+
+let totalSize = 0
 
 let tableLines = ''
 
 flags.forEach(flag => {
   const fileName = path.basename(flag)
   const {size} = fs.statSync(`${flagsFolder}/${flag}`)
+  totalSize += size
   tableLines += `<tr><td>${fileName}</td><td><img width="72" height="48" alt="" src="https://raw.githubusercontent.com/gucr/tiny-flags/main/images/${flag}"/></td><td>${size} bytes</td></tr>`
 })
 
@@ -27,11 +29,16 @@ ${tableLines}
 `
 
 const readmeContents = fs.readFileSync(readmePath, 'utf8');
-let blankReadme =  readmeContents.trim().substring(0,  readmeContents.trim().indexOf('<!-- BeginTable -->'));
+let blankReadme = readmeContents.trim().substring(0,  readmeContents.trim().indexOf('<!-- BeginTable -->'));
 
 const newContent = `
 ${blankReadme}
 <!-- BeginTable -->
+
+Average flag size: ${Math.round(totalSize / flags.length + 1)} bytes
+
+Total size: ${Math.round(totalSize / 1024)}KB
+
 ${tableHtml}
 `
 
