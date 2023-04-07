b7800c917502361291c6b332804c164e1044991f
Guillaume Cier <g@cier.fr>
2023-04-07 13:21:17 +0800

    Basic script to generate flags list in README.md

---
+++ ci/listFlags.js
@@ -0,0 +1,40 @@
+// Temporary script to generate the flags list in the README file
+// @todo replace with github actions
+
+const path = require('path')
+const fs = require('fs')
+
+const flagsFolder = path.join(__dirname, '../images')
+const readmePath = path.join(__dirname, '../README.md')
+
+const files = fs.readdirSync(flagsFolder)
+
+const flags = files.filter(file => path.extname(file).toLowerCase() === '.svg')
+
+let tableLines = ''
+
+flags.forEach(flag => {
+  const fileName = path.basename(flag)
+  const {size} = fs.statSync(`${flagsFolder}/${flag}`)
+  tableLines += `<tr><td>${fileName}</td><td><img width="72" height="48" alt="" src="https://raw.githubusercontent.com/gucr/tiny-flags/main/images/${flag}"/></td><td>${size} bytes</td></tr>`
+})
+
+const tableHtml = `
+<table>
+<tr><th>Country</th><th>Preview</th><th>Size</th></tr>
+${tableLines}
+</table>
+`
+
+const readmeContents = fs.readFileSync(readmePath, 'utf8');
+let blankReadme =  readmeContents.trim().substring(0,  readmeContents.trim().indexOf('<!-- BeginTable -->'));
+
+const newContent = `
+${blankReadme}
+<!-- BeginTable -->
+${tableHtml}
+`
+
+fs.writeFile(path.join(readmePath, '../README.md'), newContent, (err) => {
+  if (err) throw err
+})
