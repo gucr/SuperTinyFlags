db2ef39e0254e430ca43450392b8aa8d4923a137
Guillaume Cier <g@cier.fr>
2023-04-17 23:19:09 +0800

    Adding DK flag

--- flags/dk.tsx
+++ flags/dk.tsx
@@ -4,7 +4,10 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import Square from '../tools/Square'
+import Cross from '../tools/Cross'
 
 export default () => <SvgWrapper>
-  
+  <Square fill="#C60C30"/>
+  <Cross x={239.8} strokeWidth={68} stroke="#fff" />
 </SvgWrapper>
