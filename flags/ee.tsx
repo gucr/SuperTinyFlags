8ad7977fa65f773b0a565d4b3e8db0790ffe1cd1
Guillaume Cier <g@cier.fr>
2023-04-17 23:27:45 +0800

    Adding EE flag

--- flags/ee.tsx
+++ flags/ee.tsx
@@ -4,7 +4,8 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import StripesHorizontal from '../tools/StripesHorizontal'
 
 export default () => <SvgWrapper>
-  
+  <StripesHorizontal colors={["#1791ff","#000","#fff"]}/>
 </SvgWrapper>
