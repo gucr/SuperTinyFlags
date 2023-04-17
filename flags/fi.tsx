79e6b139d893058e864d36fb5f74fddb7298b1b3
Guillaume Cier <g@cier.fr>
2023-04-17 23:54:38 +0800

    Adding FI flag

--- flags/fi.tsx
+++ flags/fi.tsx
@@ -4,7 +4,10 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import Square from '../tools/Square'
+import Cross from '../tools/Cross'
 
 export default () => <SvgWrapper>
-  
+    <Square fill="#fff"/>
+  <Cross x={284} strokeWidth={130} stroke="#002f6c" />
 </SvgWrapper>
