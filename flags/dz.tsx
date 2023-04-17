84dfd3b2d2ce528ddb0432ff00f352964aec955a
Guillaume Cier <g@cier.fr>
2023-04-17 23:27:50 +0800

    Adding DZ flag

--- flags/dz.tsx
+++ flags/dz.tsx
@@ -4,7 +4,9 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import StripesVertical from '../tools/StripesVertical'
 
 export default () => <SvgWrapper>
-
+  <StripesVertical colors={['#063', '#fff']}/>
+  <path fill="#d21034" d="M464 180a120 120 0 100 120 96 96 0 110-120m5 60-109-35 67 92V183l-67 92z"/>
 </SvgWrapper>
