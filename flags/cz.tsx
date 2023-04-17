fb9310cf7ef3d2651ab8cb75cd2213f9ed4ff16c
Guillaume Cier <g@cier.fr>
2023-04-17 23:12:24 +0800

    Adding CZ flag

--- flags/cz.tsx
+++ flags/cz.tsx
@@ -4,7 +4,10 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import Square from '../tools/Square'
+import StripesHorizontal from '../tools/StripesHorizontal'
 
 export default () => <SvgWrapper>
-  
+  <StripesHorizontal colors={["#fff","#d7141a"]}/>
+  <path fill="#11457e" d="M360 240 0 0v480z"/>
 </SvgWrapper>
