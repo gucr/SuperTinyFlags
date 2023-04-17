f13ab7387e44a13eb50b8f81b4de07664816074c
Guillaume Cier <g@cier.fr>
2023-04-17 23:08:04 +0800

    Adding CO flag

--- flags/co.tsx
+++ flags/co.tsx
@@ -4,7 +4,10 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import StripesHorizontal from '../tools/StripesHorizontal'
+import Square from '../tools/Square'
 
 export default () => <SvgWrapper>
-
+  <Square fill="#ffe800"/>
+<StripesHorizontal y1={240} colors={["#00148e","#da0010"]}/>
 </SvgWrapper>
