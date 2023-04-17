b1aef682afc7e75c2fe7e9ed5e078f7b0360eb01
Guillaume Cier <g@cier.fr>
2023-04-17 23:19:03 +0800

    Adding DJ flag

--- flags/dj.tsx
+++ flags/dj.tsx
@@ -4,7 +4,11 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import StripesHorizontal from '../tools/StripesHorizontal'
+import PentaStar from '../templates/PentaStar'
 
 export default () => <SvgWrapper>
-  
+    <StripesHorizontal colors={["#6ab2e7","#12ad2b"]}/>
+  <path fill="#fff" d="M415 240 0 0v480z"/>
+  <PentaStar fill="#d7141a" cx={159} cy={246} r={132}/>
 </SvgWrapper>
