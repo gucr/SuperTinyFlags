c385039ab186639a7680ba047724c420c9e051a2
Guillaume Cier <g@cier.fr>
2023-04-18 00:02:44 +0800

    Adding GA flag

--- flags/ga.tsx
+++ flags/ga.tsx
@@ -4,7 +4,8 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import StripesHorizontal from '../tools/StripesHorizontal'
 
 export default () => <SvgWrapper>
-
+<StripesHorizontal colors={["#36a100","#ffe700","#006dbc"]}/>
 </SvgWrapper>
