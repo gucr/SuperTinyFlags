327ab323f4a5e79e1425ac8b6c84c8696f2269e0
Guillaume Cier <g@cier.fr>
2023-04-17 23:07:59 +0800

    Adding CI flag

--- flags/ci.tsx
+++ flags/ci.tsx
@@ -4,7 +4,8 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import StripesVertical from '../tools/StripesVertical'
 
 export default () => <SvgWrapper>
-
+  <StripesVertical colors={["#f77f00","#fff","#009e60"]}/>
 </SvgWrapper>
