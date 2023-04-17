b4ffc96b677b6600b04e5951086eefcf1b03a18f
Guillaume Cier <g@cier.fr>
2023-04-18 00:01:47 +0800

    Adding FO flag

--- flags/fo.tsx
+++ flags/fo.tsx
@@ -4,7 +4,11 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import Square from '../tools/Square'
+import Cross from '../tools/Cross'
 
 export default () => <SvgWrapper>
-
+  <Square fill="#fff"/>
+  <Cross x={240} strokeWidth={120} stroke="#005eb8"/>
+  <Cross x={240} strokeWidth={60} stroke="#ef3340"/>
 </SvgWrapper>
