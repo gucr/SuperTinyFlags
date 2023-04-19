d4f1cfb2d9f6ec4f40c81e2a445b8b768a0fbb25
Guillaume Cier <g@cier.fr>
2023-04-19 23:24:20 +0800

    Adding NG flag

--- flags/ng.tsx
+++ flags/ng.tsx
@@ -4,7 +4,8 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import StripesVertical from '../tools/StripesVertical'
 
 export default () => <SvgWrapper>
-
+  <StripesVertical colors={["#008753","#fff","#008753"]}/>
 </SvgWrapper>
