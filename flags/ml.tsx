ac37974eadb493061828d5034da4ed3569595f58
Guillaume Cier <g@cier.fr>
2023-04-19 23:17:48 +0800

    Adding ML flag

--- flags/ml.tsx
+++ flags/ml.tsx
@@ -4,7 +4,8 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import StripesVertical from '../tools/StripesVertical'
 
 export default () => <SvgWrapper>
-
+<StripesVertical colors={["#009a00","#ff0","red"]}/>
 </SvgWrapper>
