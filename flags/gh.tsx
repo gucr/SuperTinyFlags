2cc920a45a61295db943865a9427d53335419bbe
Guillaume Cier <g@cier.fr>
2023-04-19 23:06:19 +0800

    Adding GH flag

--- flags/gh.tsx
+++ flags/gh.tsx
@@ -4,7 +4,10 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import StripesHorizontal from '../tools/StripesHorizontal'
+import PentaStar from '../templates/PentaStar'
 
 export default () => <SvgWrapper>
-
+  <StripesHorizontal colors={['#ce1126', '#fcd116', '#006b3f']}/>
+  <PentaStar r={177} cy={248}/>
 </SvgWrapper>
