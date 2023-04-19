311d5572749af16b690292c87899b6fd6d6a71b4
Guillaume Cier <g@cier.fr>
2023-04-19 23:15:09 +0800

    Adding MA flag

--- flags/ma.tsx
+++ flags/ma.tsx
@@ -4,7 +4,11 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import Square from '../tools/Square'
+import Star from '../tools/Star'
+import PentaStar from '../templates/PentaStar'
 
 export default () => <SvgWrapper>
-
+  <Square fill="#c1272d"/>
+  <PentaStar fill="none" stroke="#006233" stroke-width="11" r={202}/>
 </SvgWrapper>
