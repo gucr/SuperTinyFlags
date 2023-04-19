7ec2aabe3b926714410ede1c232588a648efa485
Guillaume Cier <g@cier.fr>
2023-04-19 15:10:09 +0800

    Adding CH flag

--- flags/ch.tsx
+++ flags/ch.tsx
@@ -4,7 +4,9 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import Square from '../tools/Square'
 
 export default () => <SvgWrapper>
-  
+  <Square fill="#da291c"/>
+  <path fill="#fff" d="M314 90h90v105h105v90H404v105h-90V285H209v-90h105z"/>
 </SvgWrapper>
