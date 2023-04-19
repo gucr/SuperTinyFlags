b48c18fa62340cc5f4c948fa423fce65239b654a
Guillaume Cier <g@cier.fr>
2023-04-19 23:12:17 +0800

    Adding LU flag

--- flags/lu.tsx
+++ flags/lu.tsx
@@ -4,7 +4,8 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import StripesHorizontal from '../tools/StripesHorizontal'
 
 export default () => <SvgWrapper>
-
+  <StripesHorizontal colors={["#ed2939","#fff","#00a1de"]}/>
 </SvgWrapper>
