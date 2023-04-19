4cd72970c58417714af3d1c038b2ef9530c0ca12
Guillaume Cier <g@cier.fr>
2023-04-19 23:26:10 +0800

    Adding NL flag

--- flags/nl.tsx
+++ flags/nl.tsx
@@ -4,7 +4,8 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import StripesHorizontal from '../tools/StripesHorizontal'
 
 export default () => <SvgWrapper>
-  
+  <StripesHorizontal colors={["#ae1c28","#fff","#21468b"]}/>
 </SvgWrapper>
