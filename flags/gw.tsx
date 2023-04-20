a1fdbbd60cfbc8e16423508713ad1a0df5b4d4cd
Guillaume Cier <g@cier.fr>
2023-04-20 09:34:13 +0800

    Adding GW flag

--- flags/gw.tsx
+++ flags/gw.tsx
@@ -4,7 +4,12 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import StripesHorizontal from '../tools/StripesHorizontal'
+import Square from '../tools/Square'
+import PentaStar from '../templates/PentaStar'
 
 export default () => <SvgWrapper>
-  
+  <StripesHorizontal colors={["#fcd116","#009e49"]}/>
+  <Square fill="#ce1126" x2={282}/>
+  <PentaStar cx={141} r={159}/>
 </SvgWrapper>
