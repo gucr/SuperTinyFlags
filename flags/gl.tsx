d49ac5b0dd61b26e5a6e67fc81b67e4d18e7350c
Guillaume Cier <g@cier.fr>
2023-04-20 09:30:30 +0800

    Adding GL flag

--- flags/gl.tsx
+++ flags/gl.tsx
@@ -4,7 +4,9 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import Square from '../tools/Square'
 
 export default () => <SvgWrapper>
-  
+ <Square fill="#fff"/>
+  <path fill="#c8102e" d="M0 240h720v240H0zm440-1a160 160 0 100 2"/>
 </SvgWrapper>
