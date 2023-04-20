e25b9091829332f136e476bacbb39bbb0cc03c21
Guillaume Cier <g@cier.fr>
2023-04-20 09:30:34 +0800

    Adding GR flag

--- flags/gr.tsx
+++ flags/gr.tsx
@@ -4,7 +4,9 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import Square from '../tools/Square'
 
 export default () => <SvgWrapper>
-  
+  <Square fill="#0d5eaf"/>
+  <path fill="none" stroke="#fff" strokeWidth="53.3" d="M133 0v293M0 133h267m0-53h453M267 187h453M0 293h720M0 400h720"/>
 </SvgWrapper>
