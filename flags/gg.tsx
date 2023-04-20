aad83b758b0a65ffe394dc39240614f02aff422d
Guillaume Cier <g@cier.fr>
2023-04-20 09:30:26 +0800

    Adding GG flag

--- flags/gg.tsx
+++ flags/gg.tsx
@@ -4,7 +4,11 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import Square from '../tools/Square'
+import Cross from '../tools/Cross'
 
 export default () => <SvgWrapper>
-  
+  <Square fill="#fff"/>
+  <Cross strokeWidth={120} stroke="#e8112d"/>
+  <path fill="#f9dd16" d="m180 280 20-20h140v140l-20 20h80l-20-20V260h140l20 20v-80l-20 20H380V80l20-20h-80l20 20v140H200l-20-20z"/>
 </SvgWrapper>
