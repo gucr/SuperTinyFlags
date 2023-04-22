bcfbf07bfa0159119ef39cd88602f78de6871eb3
Guillaume Cier <g@cier.fr>
2023-04-22 18:58:30 +0800

    Adding HU flag

--- flags/hu.tsx
+++ flags/hu.tsx
@@ -4,7 +4,8 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import StripesHorizontal from '../tools/StripesHorizontal'
 
 export default () => <SvgWrapper>
-  
+  <StripesHorizontal colors={["#d43516","#fff","#388d00"]}/>
 </SvgWrapper>
