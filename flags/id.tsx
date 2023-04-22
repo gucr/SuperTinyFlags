9e10d30c9a56d388db19b8b2f26d80d6f3d1286e
Guillaume Cier <g@cier.fr>
2023-04-22 18:58:34 +0800

    Adding ID flag

--- flags/id.tsx
+++ flags/id.tsx
@@ -4,7 +4,9 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import StripesHorizontal from '../tools/StripesHorizontal'
 
 export default () => <SvgWrapper>
-
+  <StripesHorizontal colors={['#e70011', '#fff']}/>
 </SvgWrapper>
+
