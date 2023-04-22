615f5549bd222b640afe38c914f338078d4388a3
Guillaume Cier <g@cier.fr>
2023-04-22 19:50:28 +0800

    Allowing null colors to bypass stripes (to let the background visible)

--- tools/StripesHorizontal.tsx
+++ tools/StripesHorizontal.tsx
@@ -5,7 +5,7 @@ import {ReactNode, SVGProps} from 'react'
 
 interface Props extends SVGProps<SVGGElement> {
   /* An array containing all the successive colors.*/
-  colors: string[]
+  colors: string|null[]
   /* If you wish to repeat the stripes for a greater amount than there is colors (eg. US flag) */
   count?: number
   /* A children SVG node that will be inserted at the end of the <g> group */
@@ -23,13 +23,17 @@ interface Props extends SVGProps<SVGGElement> {
 export default ({colors = [], count, children, x1 = 0, x2 = flag.width, y1 = 0, y2 = flag.height, ...props}: Props) => {
 
   const numberOfStripes = count || colors.length
-  const stripeHeight = Math.ceil((y2 - y1) / numberOfStripes)
+  const stripeHeight = Math.round((y2 - y1) / numberOfStripes *10) / 10
 
   const pathsByColor: { [key: string]: string[] } = {};
 
   [...Array(numberOfStripes)].forEach((_, i) => {
     const y = (stripeHeight * i + stripeHeight / 2) + y1
     const color = colors[i % colors.length]
+
+    if(color === null) {
+      return
+    }
 
     if (!pathsByColor[color]) {
       pathsByColor[color] = []
