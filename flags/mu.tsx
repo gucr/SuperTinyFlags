2a1e29f3a9b9432b3c8c424182178564fe28ca22
Guillaume Cier <g@cier.fr>
2023-04-22 20:10:02 +0800

    Adding MU flag

--- flags/mu.tsx
+++ flags/mu.tsx
@@ -4,7 +4,9 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import StripesHorizontal from '../tools/StripesHorizontal'
 
 export default () => <SvgWrapper>
+  <StripesHorizontal colors={["#EA2839","#1A206D","#FFD500","#00A551"]}/>
 
 </SvgWrapper>
