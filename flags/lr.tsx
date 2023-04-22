30e6e2bd0ab1873cbe71cdc1d2e00834e4d5dc8b
Guillaume Cier <g@cier.fr>
2023-04-22 19:57:58 +0800

    Adding LR flag

--- flags/lr.tsx
+++ flags/lr.tsx
@@ -4,7 +4,13 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import StripesHorizontal from '../tools/StripesHorizontal'
+import Square from '../tools/Square'
+import PentaStar from '../templates/PentaStar'
 
-export default () => <SvgWrapper>
-
+export default () => <SvgWrapper fill="#fff">
+  <Square/>
+  <StripesHorizontal colors={["#BF0A30",null]} count={11}/>
+  <Square fill="#002868" x2={218.1} y2={218.1}/>
+  <PentaStar cx={109} cy={109} r={132}/>
 </SvgWrapper>
