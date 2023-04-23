55d20f21c7631baefcbbf4570ccd7e1d1b456306
Guillaume Cier <g@cier.fr>
2023-04-23 20:30:53 +0800

    Adding JP flag

--- flags/jp.tsx
+++ flags/jp.tsx
@@ -4,7 +4,10 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import Square from '../tools/Square'
+import Circle from '../tools/Circle'
 
 export default () => <SvgWrapper>
-
+  <Square fill="#fff"/>
+  <Circle r={144} fill="#bc002d"/>
 </SvgWrapper>
