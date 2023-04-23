1b9114195950a4fd78fbb9a7889789d645c1a2d7
Guillaume Cier <g@cier.fr>
2023-04-23 20:30:58 +0800

    Adding RU flag

--- flags/ru.tsx
+++ flags/ru.tsx
@@ -4,7 +4,8 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import StripesHorizontal from '../tools/StripesHorizontal'
 
 export default () => <SvgWrapper>
-
+  <StripesHorizontal colors={["#fff","#0039a6","#d52b1e"]}/>
 </SvgWrapper>
