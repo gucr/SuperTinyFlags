65d7fde6dfa009e703b7dc4a34c893e7b772b520
Guillaume Cier <g@cier.fr>
2023-04-23 18:12:55 +0800

    Adding NR flag

--- flags/nr.tsx
+++ flags/nr.tsx
@@ -4,7 +4,11 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import Square from '../tools/Square'
+import Star from '../tools/Star'
 
 export default () => <SvgWrapper>
-
+  <Square fill="#002B7F"/>
+  <Square fill="#FFC61E" y1={220.5} y2={260.6}/>
+  <Star ends={12} diameter={160} innerDiameter={80} x={190} y={340} fill="#fff"/>
 </SvgWrapper>
