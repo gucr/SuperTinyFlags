804ffafb7a7fd9e79ba784c240bee121b0bef2b7
Guillaume Cier <g@cier.fr>
2023-04-23 18:22:17 +0800

    Adding PE flag

--- flags/pe.tsx
+++ flags/pe.tsx
@@ -4,7 +4,8 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import StripesVertical from '../tools/StripesVertical'
 
 export default () => <SvgWrapper>
-
+  <StripesVertical colors={["#D91023","#fff"]} count={3}/>
 </SvgWrapper>
