cfd7d4b93c690daf1acd333f50f7bf81b7cbba1e
Guillaume Cier <g@cier.fr>
2023-04-23 18:12:48 +0800

    Adding PA flag

--- flags/pa.tsx
+++ flags/pa.tsx
@@ -4,7 +4,14 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import Square from '../tools/Square'
+import PentaStar from '../templates/PentaStar'
+import Compound from '../tools/Compound'
+import Star from '../tools/Star'
 
 export default () => <SvgWrapper>
-
+  <Square fill="#072357" x2={360}/>
+  <Square fill="#DA121A" x1={360}/>
+  <path id="pa-a" fill="#fff" d="M0 0H360V240H0ZM180 60 167 101H123L158 127 145 168 180 143 215 168 202 127 238 101H194Z"/>
+  <use href="#pa-a" x={360} y={240}/>
 </SvgWrapper>
