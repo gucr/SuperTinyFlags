eaebc9e466a3fbd1212d8340809edf1a33cca8b0
Guillaume Cier <g@cier.fr>
2023-04-23 10:37:39 +0800

    Adding NO flag

--- flags/no.tsx
+++ flags/no.tsx
@@ -4,7 +4,11 @@
  */
 import React from 'react'
 import SvgWrapper from '../tools/SvgWrapper'
+import Square from '../tools/Square'
+import Cross from '../tools/Cross'
 
 export default () => <SvgWrapper>
-  
+  <Square fill="#EF2B2D"/>
+  <Cross x={240} stroke="#FFF" strokeWidth={120}/>
+  <Cross x={240} stroke="#002868" strokeWidth={60}/>
 </SvgWrapper>
